693086d26b3f90e2b695f062f5fc5439c43ed12c
Momina Tufail <momnatufail30@gmail.com>
2024-12-30 01:55:24 +0500

    code updated

--- lib/View/auth-view/signup-view/signup-view.dart
+++ lib/View/auth-view/signup-view/signup-view.dart
@@ -1,17 +1,30 @@
 import 'package:architecture/Controller/constants/app-colors/app-colors.dart';
+import 'package:architecture/Controller/constants/app-loader/app-loader.dart';
 import 'package:architecture/Controller/widgets/blacktext-heading-widget.dart';
 import 'package:architecture/Controller/widgets/button-widget.dart';
 import 'package:architecture/Controller/widgets/image-widget.dart';
 import 'package:architecture/Controller/widgets/normal-text-widget.dart';
 import 'package:architecture/Controller/widgets/textformfield-widget.dart';
+import 'package:architecture/View/auth-view/signin-view/signin-view.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SignupView extends StatelessWidget {
+class SignupView extends StatefulWidget {
   SignupView({super.key});
+
+  @override
+  State<SignupView> createState() => _SignupViewState();
+}
+
+class _SignupViewState extends State<SignupView> {
   TextEditingController fullNameController= TextEditingController();
+
   TextEditingController emailController= TextEditingController();
+
   TextEditingController passwordController= TextEditingController();
+
+  bool isloading= false;
 
   @override
   Widget build(BuildContext context) {
@@ -27,15 +40,33 @@ class SignupView extends StatelessWidget {
         //TextFormFieldWidget(hintText: 'Confirm your Password', controller:fullNameController),
          SizedBox(height: 10,),
 
-        ButtonWidget(text: 'SignUp', ontap: ()async{
-          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        isloading?AppLoader():ButtonWidget(
+            text: 'SignUp',
+            ontap: () async {
+              isloading = true;
+              setState(() {});
+
+
+          await FirebaseAuth.instance.
+          createUserWithEmailAndPassword(
               email: emailController.text.trim(),
-              password: passwordController.text);
+              password: passwordController.text).then((value) {
+                isloading = false;
+                setState(() {});
+                Navigator.push(context, CupertinoPageRoute(
+                    builder: (context) => LoginView()));
+                
 
-          
+              });
 
-        }),
+
+              }
+
+
+        ),
+
         SizedBox(height: 10,),
+
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
             children: [
