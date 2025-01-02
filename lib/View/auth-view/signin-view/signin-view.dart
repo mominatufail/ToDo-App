beb3ff755fe5a2b41255a96ee2e39898fa1f5469
Momina Tufail <momnatufail30@gmail.com>
2025-01-02 09:53:17 +0500

    code updated

--- lib/View/auth-view/signin-view/signin-view.dart
+++ lib/View/auth-view/signin-view/signin-view.dart
@@ -8,11 +8,23 @@ import 'package:flutter/material.dart';
  }
  
  class _LoginViewState extends State<LoginView> {
-   TextEditingController
-
+   TextEditingController emailController= TextEditingController();
+   TextEditingController passwordController= TextEditingController();
+   bool isLoading = false;
 
    @override
    Widget build(BuildContext context) {
-     return const Placeholder();
+     return Scaffold(
+       body: Column(
+         children: [
+           TextFormField(
+             controller: ,
+
+           )
+
+         ],
+       ),
+
+     );
    }
  }
